81599b12b81e6f88941a82e85ff3533b914b9591
Nannykko <144710787+Nannykko@users.noreply.github.com>
2024-01-11 20:19:11 +0300

    Program.cs
    Program.cs

---
+++ vladislav-semkin-kt-41-20/Program.cs
@@ -0,0 +1,51 @@
+using NLog;
+using NLog.Web;
+using Microsoft.EntityFrameworkCore;
+using vladislav_semkin_kt_41_20.Data;
+using vladislav_semkin_kt_41_20.MiddleWares;
+
+using static vladislav_semkin_kt_41_20.ServiceExtensions.ServiceExtensions;
+using Microsoft.AspNetCore.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+// Add services to the container.
+try
+{
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddDbContext<TeachersDbContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddServices();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    app.UseMiddleware<ExceptionHandlerMiddleware>();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+}
+
+catch (Exception ex)
+{
+    logger.Error(ex, "Exception!");
+}
+finally
+{
+    LogManager.Shutdown();
+}
